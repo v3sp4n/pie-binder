e8de2d5f0ad92153a8b86aacbba01e6b26103e78
v3sp4n <57196133+v3sp4n@users.noreply.github.com>
2023-04-04 09:19:59 -0800

    Create downloander.lua

---
+++ downloander.lua
@@ -0,0 +1,25 @@
+{
+  ['SAMP.lua'] = {
+    newVersion = 'install if there is no',
+    to = '/lib',
+    repo = 'THE-FYP/SAMP.Lua',
+    files = {
+      ["samp.events"] = "samp/events.lua",
+      ["samp.events.bitstream_io"] = "samp/events/bitstream_io.lua",
+      ["samp.events.core"] = "samp/events/core.lua",
+      ["samp.events.extra_types"] = "samp/events/extra_types.lua",
+      ["samp.events.handlers"] = "samp/events/handlers.lua",
+      ["samp.events.utils"] = "samp/events/utils.lua",
+      ["samp.raknet"] = "samp/raknet.lua",
+      ["samp.synchronization"] = "samp/synchronization.lua",
+    },
+  },
+  ['pie menu'] = {
+    newVersion = 1.00,
+    to = '/lib',
+    repo = 'v3sp4n/pie-binder',
+    files = {
+      ['imgui_piemenu(for pie binder)'] = "lib/imgui_piemenu(for pie binder).lua",
+    },
+  },
+}
